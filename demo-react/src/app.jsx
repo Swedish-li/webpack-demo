152abd2750b8a66389c5ae3271a38d5a765f6011
Swedish-li <one.piece.li.2016@gmail.com>
2018-05-27 23:57:18 +0800

    add eslint jsx,react dependencies

--- demo-react/src/app.jsx
+++ demo-react/src/app.jsx
@@ -3,14 +3,14 @@ import Clock from './clock'
 import Toggle from './toggle'
 
 export default class Greeting extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello,{this.props.name}</h1>
-                <Clock />
-                <Toggle />
-            </div>
+  render () {
+    return (
+      <div>
+        <h1>Hello,{this.props.name}</h1>
+        <Clock />
+        <Toggle />
+      </div>
 
-        )
-    }
+    )
+  }
 }
